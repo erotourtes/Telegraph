e867cee13613f5127c6e426ed09474194006f238
Max Siryk <erotourtes@gmail.com>
2023-09-01 15:42:01 +0300

    Updated start script

--- start.sh
+++ start.sh
@@ -4,6 +4,13 @@
 # "VITE_REACT_APP_BASE_URL=http://backend:8000\n\
 # VITE_REACT_APP_WS_URL=ws://backend:8000" >> ./frontend/telegraph/.env
 
+# check for docker-compose existance
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose could not be found"
+    exit
+fi
+
 node ./replace.env.mjs
 docker-compose -f ./docker-compose.local.yml up --build
 
